3603a5db193154717b8a827571a94d1a8d3c8bd2
pessoadavi <pessoadavi@gmail.com>
2022-07-19 14:07:14 -0300

    Implementando o rabbitMQ e criando o subscriber do mscliente para a emissão de cartão.

--- msclient/src/main/java/com/project/cursomicrosservicos/msclient/MsclientApplication.java
+++ msclient/src/main/java/com/project/cursomicrosservicos/msclient/MsclientApplication.java
@@ -1,9 +1,11 @@
 package com.project.cursomicrosservicos.msclient;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableRabbit
 @EnableEurekaClient
 @SpringBootApplication
 public class MsclientApplication {
